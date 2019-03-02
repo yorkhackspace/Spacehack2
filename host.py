b0fa237b0ec4ea105db825ed4fb2a6773b72c152
Nick Moriarty <njmoriarty@gmail.com>
2019-03-02 16:33:03 +0000

    Add gamestarter integration

--- host.py
+++ host.py
@@ -2,6 +2,8 @@
 from paho.mqtt import client as mqtt
 import signal
 import sys
+from GameStarter.gamestart import GameStarter
+import time
 
 class MqttWrapper:
     def __init__(self, address='localhost', port=1883):
@@ -25,6 +27,12 @@ class MqttWrapper:
         self.mqtt.subscribe(topic)
         self.mqtt.message_callback_add(topic, callback)
 
+    def pub(self, topic, payload):
+        self.mqtt.publish(topic, payload)
+
+    def dump(self, client, userdata, message):
+        print("%s: %s" % (message.topic, message.payload.decode('utf-8')))
+
 class SpacehackHost:
 
     def __init__(self, mqtt=MqttWrapper()):
@@ -33,9 +41,28 @@ class SpacehackHost:
     def test_topic(self, client, userdata, message):
         print(message)
 
+    def handle_lobby_join(self, client, userdata, message):
+        _, console, *_ = message.topic.split('/')
+        player_index = int(console)
+        payload = message.payload.decode('utf-8')
+        print("Player %d: %s" % (player_index, payload))
+        if payload == '1':
+            self.g.push(player_index)
+        elif payload == '0':
+            self.g.release(player_index)
+
     def main(self):
         self.mqtt.connect()
         self.mqtt.sub('test', self.test_topic)
+        self.g = GameStarter(4, 2.0, 5.0, 1.0)
+        self.mqtt.sub('spacehack/+/join', self.handle_lobby_join)
+
+    def gamestart_loop(self):
+        while(not self.g.shouldStart()):
+            time.sleep(0.05)
+            self.g.timeStep(0.05)
+        self.mqtt.pub('spacehack/start', self.g.totalStartablePlayers())
+        print("Total startable players: %d" % self.g.totalStartablePlayers())
 
     def stop(self):
         self.mqtt.stop()
@@ -48,5 +75,6 @@ if __name__ == '__main__':
         sys.exit(0)
     signal.signal(signal.SIGINT, signal_handler)
     sh.main()
+    sh.gamestart_loop()
     while True:
         signal.pause()
