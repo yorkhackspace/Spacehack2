60248c9cd55a4b6146a2ff24bf55e3bf0bc5ad9f
Daniel Bailey <danieljabailey@gmail.com>
2019-03-09 17:56:21 +0000

    Extract mqtt wrapper to another file

--- host.py
+++ host.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-import os
 from paho.mqtt import client as mqtt
+from mqtt_wrapper import MqttWrapper
 import signal
 import sys
 from GameStarter import GameStarter
@@ -13,48 +13,6 @@ class LobbyConfiguration:
     JOIN_GAME_DELAY = 1.0
     LEAVE_GAME_DELAY = 0.5
 
-class MqttWrapper:
-    def __init__(self, address='localhost', port=1883, topic_prefix='spacehack/'):
-        self.address = address
-        self.port = port
-        self.topic_prefix = topic_prefix
-        override = 'SH_TOPIC_PREFIX_OVERRIDE'
-        if override in os.environ:
-            prefix = os.environ[override]
-            self.topic_prefix = ''.join(c if c not in '\/.#$+_' else '-' for c in os.environ[override]) + '/'
-
-    def on_connect(self, client, userdata, flags, rc):
-        print("connected to mqtt")
-
-    def connect(self):
-        self.mqtt = mqtt.Client()
-        self.mqtt.on_connect = self.on_connect
-        self.mqtt.connect(self.address, self.port, 60)
-        self.mqtt.loop_start()
-
-    def stop(self):
-        self.mqtt.loop_stop()
-        self.mqtt.disconnect()
-
-    def sub(self, topic, callback):
-        print('sub', topic)
-        full_topic = self.topic_prefix + topic
-        self.mqtt.subscribe(full_topic)
-        wrapped_callback = self.wrap_message_handler(callback)
-        self.mqtt.message_callback_add(full_topic, wrapped_callback)
-
-    def wrap_message_handler(self, callback):
-        @wraps(callback)
-        def message_handler_wrapper(client, userdata, message):
-            return callback(message.topic, message.payload.decode('utf-8'))
-        return message_handler_wrapper
-
-    def pub(self, topic, payload):
-        print('pub', topic)
-        self.mqtt.publish(self.topic_prefix + topic, payload)
-
-    def dump(self, topic, payload):
-        print("%s: %s" % (topic, payload))
 
 class Service:
     def __init__(self, func, args, kwargs):
